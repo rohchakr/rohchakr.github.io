acd6f43778b49e9bc845a2d1aee2e1e294055688
Rohit Chakraborty <rohchakr@gmail.com>
2020-09-12 17:34:37 +0530

    Footer content - contact urls

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,20 +1,43 @@
 import React from "react"
 import { VscGithub } from "react-icons/vsc"
 import { FaTwitter, FaLinkedin, FaInstagram, FaFacebook } from "react-icons/fa"
+import * as Contacts from "../constants/contact-url"
 
-const Footer = () => (
-  <footer>
+const Item = ({ icon, text, url }) => (
+  <li>
+    <a href={url}>
+      {icon}{text}
+    </a>
+  </li>
+)
+
+const CopyRightArea = () => (
+  <>
     © {new Date().getFullYear()}, Built with
     {` `}
     <a href="https://www.gatsbyjs.org">Gatsby</a>
-    Connect:
-    <VscGithub />
-    <FaLinkedin />
-    <FaInstagram />
-    <FaTwitter />
-    <FaFacebook />
+  </>
+)
+
+const ContactArea = () => (
+  <>
+    <h3>Connect</h3>
+    <ul>
+      <Item icon={<VscGithub />} text='GitHub' url={Contacts.GITHUB_URL} />
+      <Item icon={<FaLinkedin />} text='LinkedIn' url={Contacts.LINKEDIN_URL} />
+      <Item icon={<FaInstagram />} text='Instagram' url={Contacts.INSTAGRAM_URL} />
+      <Item icon={<FaTwitter />} text='Twitter' url={Contacts.TWITTER_URL} />
+      <Item icon={<FaFacebook />} text='Facebook' url={Contacts.FACEBOOK_URL} />
+    </ul>
+  </>
+)
+
+const Footer = () => (
+  <footer>
+    <CopyRightArea />
+    <ContactArea />
   </footer>
 )
-    
+
 
 export default Footer
---
+++ src/constants/contact-url.js
@@ -0,0 +1,5 @@
+export const GITHUB_URL = 'https://github.com/rohchakr'
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/rohchakr'
+export const TWITTER_URL = 'https://twitter.com/rohchakr'
+export const INSTAGRAM_URL = 'https://www.instagram.com/rohchakr'
+export const FACEBOOK_URL = 'https://www.facebook.com/rcredhit'
