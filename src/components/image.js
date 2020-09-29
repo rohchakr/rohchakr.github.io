a114d8e496d3fb62e9d6407a6130af833b34607c
Rohit Chakraborty <rohchakr@gmail.com>
2020-09-29 19:25:05 +0530

    Replace gatsby-astranaut with own image

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "rohchakr-face-1-2k20.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
