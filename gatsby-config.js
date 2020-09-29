dcf0fc33651c0940c963bb857b002943b3327c75
Rohit Chakraborty <rohchakr@gmail.com>
2020-09-29 20:00:51 +0530

    New icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#502505`,
         theme_color: `#502505`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/rohchakr-icon-1.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
