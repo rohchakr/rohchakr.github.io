c3612e01a19933e18d9fd137a01d229201ea48ac
Rohit Chakraborty <rohchakr@gmail.com>
2020-10-10 20:05:16 +0530

    Add svg

--- src/pages/ipl-2020.js
+++ src/pages/ipl-2020.js
@@ -1,21 +1,30 @@
-import React from "react"
+import React, { useEffect } from "react"
 import * as d3 from "d3"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import IPLData from "../data/ipl-2020-data.json"
 
-const IPLTeamViz = () => {
+const createViz = (iplData) => {
     console.log(d3.version)
-    const iplData = IPLData
-    
+    console.log(iplData)
+    var svg = d3.select('#svgWrapper').append('svg')
+        .attr('width',500).attr('height',500)
+        .style('border', '1px solid grey')
+}
+
+const IPLTeamViz = () => {
+    useEffect(
+        () => createViz(IPLData),
+        []
+    )
+
     return (
         <Layout navLocation="misc">
-            <SEO title="Blog" />
-            {
-                console.log(iplData)
-            }
+            <SEO title="IPL 2020 Team Viz" />
+            <div id='svgWrapper'></div>
         </Layout>
-)}
+    )
+}
 
 export default IPLTeamViz
