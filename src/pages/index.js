4269ba628fa6c8cc95e5da4da5c4efa907359d8b
Rohit Chakraborty <rohchakr@gmail.com>
2020-12-21 12:32:30 +0530

    Index page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout navLocation="home">
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <h1>Hi,</h1>
+    <p>Greetings from Rohit Chakraborty! So glad to see you here.</p>
+    <p>This website is intended to share my professional journey, technical how-to's, lifestyle preferences and general life experience. It also helps me getting more organized, perform experiments and improve my Web Development, Automation and Product Strategy and Communication skills.</p>
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </div> */}
+    {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
+    {/* <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
 
