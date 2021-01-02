6f7303014e0f55ebb8aa1406c2d392991288457f
Rohit Chakraborty <rohchakr@gmail.com>
2021-01-02 13:43:23 +0530

    Add resume link and final career timeline for v1

--- src/pages/career-timeline.js
+++ src/pages/career-timeline.js
@@ -9,26 +9,28 @@ const IndexPage = () => (
   <Layout navLocation="career-timeline">
     <SEO title="Career Timeline" />
 
+    <a href="https://rohchakr.github.io/resume/RohitChakraborty_Resume.pdf" target="_blank"><button className="resumeButton">Resume</button></a>
+
     <h5>2020</h5>
-    <p>Learnt data visualization concepts. Designed and developed advanced visualizations using d3.js.</p>
+    <p>Invested more in code quality and design patterns. Learnt data visualization concepts. Interacted more with the strategy team. Designed and developed advanced visualizations for Oracle Construction Intelligence Cloud. My first github PR got merged. Started working for my website.</p>
 
     <h5>2019</h5>
-    <p>Introduced to full-stack web development based on JavaEE-JSP stack. Fixed security vulnerabilities.</p>
+    <p>Introduced to full-stack web development based on JavaEE-JSP stack. Actively participated in code-reviews and documentation. Fixed security vulnerabilities. Got a better understanding of the Software Development life-cycle and various cross-teams involved with it. Contributed towards debugging and fixing production issues.</p>
 
     <h5>2018</h5>
-    <p>Started job at Oracle as an Applications Developer. Worked with Docker and Kubernetes(minikube).</p>
+    <p>Started job at Oracle as an Applications Developer. Worked with Docker and Kubernetes(minikube). Contributed towards development of docker image for Oracle Primavera Analytics Administration application.</p>
     
     <h5>2017</h5>
-    <p>Introduced to Machine Learning and Natural Language Processing. Carried out a research work in Twitter Network Analysis.</p>
+    <p>Introduced to Machine Learning and Natural Language Processing. Carried out a research work in Twitter Network Analysis which got published in 2019.</p>
 
     <h5>2016</h5>
-    <p>Got started with Android Development. Developed and deployed an app in Google Play Store.</p>
+    <p>Got started with Android Development. Developed and deployed an app in Google Play Store. Studied Algorithms, Graph Theory, Database Management Systems, Operating Systems and Networking. Learnt simple assembly level adders and multipliers for Intel-8085 processor.</p>
 
     <h5>2015</h5>
-    <p>Introduced to Linux and open source softwares for the first time. Learnt about dual-boot and disk partitions. Learnt basic CSS and JavaScript.</p>
+    <p>Introduced to Linux and open source softwares for the first time. Learnt about dual-boot and disk partitions. Learnt basic CSS and JavaScript. Studied Data Structures, Discrete Mathematics, Logic Circuits and Communication protocols in detail.</p>
 
     <h5>2014</h5>
-    <p>Got started with C and gcc compiler. Practised algorithmic problems in CodeChef.</p>
+    <p>Started B.Tech course in Computer Science and Engineering at National Institute of Technology, Agartala, India. Got started with C and gcc compiler. Practised algorithmic problems in CodeChef.</p>
 
     <h5>2012</h5>
     <p>Introduced to sorting algorithms - bubble sort, selection sort, insertion sort and merge sort. Completed my first application - A C++ Calender application as part of school project</p>
