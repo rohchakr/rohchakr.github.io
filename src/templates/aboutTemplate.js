979f60cb02fcd3ae2a8d463322415a2539709a02
Rohit Chakraborty <rohchakr@gmail.com>
2021-01-02 11:47:18 +0530

    Final about template for v1

--- src/templates/aboutTemplate.js
+++ src/templates/aboutTemplate.js
@@ -5,6 +5,19 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const linkStyle = {
+  color: `#f5ebc8`,
+  backgroundColor: `#643c28`,
+  textDecoration: `none`
+}
+
+const notificationStyle = {
+  color: `#f5ebc8`,
+  backgroundColor: `#321905`,
+  padding: `1.45rem`,
+  borderRadius: `3px`
+}
+
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
@@ -15,13 +28,14 @@ export default function Template({
         <SEO title="About" />
         <div className="blog-post-container">
         <div className="blog-post">
-            <h1>{frontmatter.title}</h1>
-            <h2>{frontmatter.date}</h2>
+            <p style={notificationStyle}>Please head over to the <Link to='/career-timeline/' style={linkStyle}>Career Timeline</Link> page if you are more interested in my professional journey.</p>
+            {/* <h2>{frontmatter.date}</h2> */}
+            {/* frontmatter has two more fields 'date' and 'last-edited-date' - to be used for v2*/}
             <div
             className="blog-post-content"
             dangerouslySetInnerHTML={{ __html: html }}
             />
-            <Link to="/">Go back to the homepage</Link>
+            {/* <Link to="/">Go back to the homepage</Link> */}
         </div>
         </div>
     </Layout>
