0fc86d1a32914c4df07ecc64d2401b4a81c6bbe1
rohchakr <rohchakr@gmail.com>
2023-08-07 05:18:35 +0000

    Deploying to master from  @ 85589fab8e50df331c71139edaddf1e0cdc43923 🚀

--- component---src-pages-404-js-5c4acfe45dc275823667.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(e,t,a){a.r(t);var l=a(7294),s=a(6569),u=a(9357);t.default=()=>l.createElement(s.Z,null,l.createElement(u.Z,{title:"404: Not found"}),l.createElement("h1",null,"NOT FOUND"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
-//# sourceMappingURL=component---src-pages-404-js-5c4acfe45dc275823667.js.map
--- component---src-pages-page-2-js-0b556a0517359c489e91.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{4948:function(e,t,a){a.r(t);var l=a(7294),n=a(1883),r=a(6569),c=a(9357);t.default=()=>l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-0b556a0517359c489e91.js.map
