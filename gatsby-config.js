97a61d453f5427ffe39853e736ed4b243b75706f
Rohit Chakraborty <rohchakr@gmail.com>
2023-08-11 09:55:48 +0530

    New logo : paper windmill (#36)

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
         background_color: `#502505`,
         theme_color: `#502505`,
         display: `minimal-ui`,
-        icon: `src/images/rohchakr-icon-1.png`, // This path is relative to the root of the site.
+        icon: `src/images/rohchakr_icon_512_512.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
