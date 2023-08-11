7ddd64b6ad26e6802c8d22f41b00fac8a498c9f6
rohchakr <rohchakr@gmail.com>
2023-08-11 10:11:42 +0000

    Deploying to master from  @ d5785887e852088999234b856106df967b7cdd07 🚀

---
+++ component---src-pages-404-js-3cdd0c9d9b1965733dce.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(e,t,a){a.r(t);var l=a(7294),s=a(1636),u=a(5595);t.default=()=>l.createElement(s.Z,null,l.createElement(u.H,{title:"404: Not found | rohchakr"}),l.createElement("h1",null,"NOT FOUND"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-3cdd0c9d9b1965733dce.js.map
