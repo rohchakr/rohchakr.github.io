debe2c50dbe8715182ba1596bcdcf1ab6f7e6b0e
rohchakr <rohchakr@gmail.com>
2023-08-24 18:27:42 +0530

    Add auto-anchor

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-autolink-headers`,
           {
             resolve: `gatsby-remark-images`,
             options: {
