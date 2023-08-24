018807c75e6daeec6964335fa51fe1e4bab40f5c
rohchakr <rohchakr@gmail.com>
2023-08-24 13:02:29 +0530

    Add comments border

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,23 +1,26 @@
 import React from "react"
 import Giscus from "@giscus/react"
+import * as commentsStyles from "./comments.module.css"
 
 export default function Comments() {
     return (
-        <Giscus
-            repo="rohchakr/rohchakr-comments-server"
-            repoId="R_kgDOKJb3mw"
-            category="Announcements"
-            categoryId="DIC_kwDOKJb3m84CY0lb"
-            mapping="title"
-            strict="1"
-            reactionsEnabled="1"
-            emitMetadata="0"
-            inputPosition="top"
-            theme="light"
-            lang="en"
-            loading="lazy"
-            crossorigin="anonymous"
-            asyncConfig="1"
-        />
+        <div className={commentsStyles.CommentsContainer}>
+            <Giscus
+                repo="rohchakr/rohchakr-comments-server"
+                repoId="R_kgDOKJb3mw"
+                category="Announcements"
+                categoryId="DIC_kwDOKJb3m84CY0lb"
+                mapping="title"
+                strict="1"
+                reactionsEnabled="1"
+                emitMetadata="0"
+                inputPosition="top"
+                theme="light"
+                lang="en"
+                loading="lazy"
+                crossorigin="anonymous"
+                asyncConfig="1"
+            />
+        </div>
     )
 }
