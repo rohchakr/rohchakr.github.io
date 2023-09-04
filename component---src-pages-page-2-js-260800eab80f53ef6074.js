c8361ceee0dcf1b93cced9cf1e2f97f3c681d920
rohchakr <rohchakr@gmail.com>
2023-09-04 09:35:19 +0000

    Deploying to master from  @ b824fe80fe58899c2f92b826ffcc21acaee1ffe4 🚀

---
+++ component---src-pages-page-2-js-260800eab80f53ef6074.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{4948:function(e,t,a){a.r(t);var l=a(7294),r=a(1883),n=a(7900),c=a(5595);t.default=()=>l.createElement(n.Z,null,l.createElement(c.H,{title:"Page two | rohchakr"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(r.Link,{to:"/"},"Go back to the homepage"))}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-260800eab80f53ef6074.js.map
